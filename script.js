129afa90dc5f7403f7e18a1445a860e112000ac5
grzegorztata <grzegorz.tata@gmail.com>
2018-10-29 23:25:49 +0200

    fixing JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://afternoon-taiga-43848.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://afternoon-taiga-43848.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 

8f56ee6562648f03c7bd63f3ad589556534f521e
grzegorztata <grzegorz.tata@gmail.com>
2018-11-05 23:03:05 +0200

    fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://arcane-tor-34279.herokuapp.com/v1/task/';
+  const apiRoot = 'https://arcane-tor-34279.herokuapp.com/v1/';
   const trelloApiRoot = 'https://arcane-tor-34279.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
